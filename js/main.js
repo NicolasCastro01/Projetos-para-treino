9b900b93db7e9476230c7288ede343846f4bad87
Nicolas Castro <nicolas.y0909@gmail.com>
2022-08-20 09:57:18 -0300

    Deploy!

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let labelNumber = document.getElementById('labelNumber');
+let sideBar = document.getElementById('sidebar');
+let display = document.getElementById('display');
+
+let optionLetraMaiúscula = document.getElementById('lMai');
+let optionLetraMinuscula = document.getElementById('lMin');
+let optionCaractereEspecial = document.getElementById('cEsp');
+let optionNumeros = document.getElementById('number');
+
+let arrOptions;
+
+function mudarLabel(){
+
+
+
+    labelNumber.innerText = sideBar.value;
+    let options = {
+        numbers: optionNumeros.checked ? '0123456789' : false,
+        specialsCharacteres: optionCaractereEspecial.checked ? '@/-=+`^~´{[}]>.<,:;?' : false,
+        upperCase: optionLetraMaiúscula.checked ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : false,
+        lowerCase: optionLetraMinuscula.checked ? 'abcdefghijklmnopqrstuvwxyz' : false,
+    }
+
+    
+    let arr = Object.keys(options).map((optionType)=>{
+        if(options[optionType]){
+            return options[optionType]
+        }
+    })
+
+    arrOptions = arr.filter(option => {return option != undefined}).join()
+
+    display.value = generatePassword(labelNumber.innerText)
+}
+
+function generatePassword(length){
+    return length == 0 ? '' : arrOptions.charAt(Math.floor(Math.random() * arrOptions.length)) + generatePassword(length - 1)
+}
+
+mudarLabel()
